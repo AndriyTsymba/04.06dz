eb9ad8e6a1f685241412aba75952ce2395cdaca1
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-31 20:27:14 +0300

    2

--- dev.py
+++ dev.py
@@ -1,12 +1,9 @@
-#1
+#2
 import requests
 from bs4 import BeautifulSoup
-
-response = requests.get("https://www.example.com/")
-
+response = requests.get("https://uk.wikipedia.org/")
 if response.status_code == 200:
     soup = BeautifulSoup(response.content , "html.parser")
-    h2 = soup.find_all("h2").text
-    print(h2)
-else:
-    print("No connection or no h2")
+    tablee = soup.find_all("table")
+    for i in tablee:
+        print(i)
