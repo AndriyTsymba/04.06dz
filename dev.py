efda0d502139f48eb920e729ca1aea83b3e57ec2
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-31 20:31:59 +0300

    4

--- dev.py
+++ dev.py
@@ -1,7 +1,8 @@
-#4
 import requests
 from bs4 import BeautifulSoup
+
 response = requests.get("https://www.example.com/")
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.content , "html.parser")
     h1 = soup.find_all("h1").text
