c395c0cc9da0f888e3a7f4e085cf4614e274883a
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-31 20:29:22 +0300

    3

--- dev.py
+++ dev.py
@@ -1,9 +1,13 @@
-#2
+#3
 import requests
 from bs4 import BeautifulSoup
-response = requests.get("https://uk.wikipedia.org/")
+
+response = requests.get("https://www.example.com/")
 if response.status_code == 200:
     soup = BeautifulSoup(response.content , "html.parser")
-    tablee = soup.find_all("table")
-    for i in tablee:
-        print(i)
+    elements = soup.find_all({"class": "example-class"})
+    if elements:
+        elements_name = elements.text.strip()
+        print(elements_name)
+    else:
+        print("Таких елементів немає на сторінці")
