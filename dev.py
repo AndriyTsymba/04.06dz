f9a4f73860c5b966ad534f0ffb92153adaf54a29
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-31 20:30:39 +0300

    4

--- dev.py
+++ dev.py
@@ -1,13 +1,10 @@
-#3
+#4
 import requests
 from bs4 import BeautifulSoup
-
 response = requests.get("https://www.example.com/")
 if response.status_code == 200:
     soup = BeautifulSoup(response.content , "html.parser")
-    elements = soup.find_all({"class": "example-class"})
-    if elements:
-        elements_name = elements.text.strip()
-        print(elements_name)
-    else:
-        print("Таких елементів немає на сторінці")
+    h1 = soup.find_all("h1").text
+    print(h1)
+else:
+    print("No connection or no h1")
